6dda7b58bc1c8fcba02ebaee1a8d271387063fa8
Elif Bölük <101395811+elifboluk@users.noreply.github.com>
2022-11-19 12:20:14 +0300

    Add files via upload

---
+++ RepositoryPattern.Api/Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPattern.Domain.Entities;
+using RepositoryPattern.Services.Abstracts;
+using RepositoryPattern.Services.DTOs;
+
+namespace RepositoryPattern.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+        private readonly IMapper mapper; // mapping
+
+        public ProductController(IProductService productService, IMapper mapper)
+        {
+            this.productService = productService;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult Post(ProductDto product)
+        {
+            var productDtoMapped = mapper.Map<Product>(product);
+            productService.Add(productDtoMapped);
+            return Ok();
+        }
+
+        [HttpGet("Products")]
+        public IActionResult Get()
+        {
+            var result = productService.GetAll();
+            return Ok();
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(Product product)
+        {
+            var productDtoMapped = mapper.Map<Product>(product);
+            productService.Update(productDtoMapped);
+            return Ok();
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(Product product)
+        {
+            productService.HardDelete(product);
+            return Ok();
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
+        {
+            productService.GetById(id);
+            return Ok();
+        }
+
+    }
+}
